e3cf480e6a36088bbc578152933be53bd9ffc0b2
sanika-patil02 <115109440+sanika-patil02@users.noreply.github.com>
2024-07-21 00:03:14 +0530

    Time: 22 ms (46.19%), Space: 27.4 MB (61.85%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // vector<int> arr=nums;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     nums[(i+k)%n]=arr[i];
+        // }
+        // Here time complexity=o(n)
+        // extra space =o(n)
+        
+        int n=nums.size();
+
+        if(k>=n){
+            k=k%n;
+        }
+        if(k>0){
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());   
+        }
+        // here time complexity=o(2n)
+        // extra space =o(1)
+
+    }
+};
