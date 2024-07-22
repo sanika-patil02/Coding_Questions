787853ae0e208e3589aeb80ecb3ef840ba607a9a
sanika-patil02 <115109440+sanika-patil02@users.noreply.github.com>
2024-07-22 14:45:45 +0530

    Time: 613 ms (5.11%), Space: 19.5 MB (34.37%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1=0;
+// Brute-Force
         for(int i=0;i<nums.size();i++){
-            xor1=xor1 ^ nums[i];
+            int num=nums[i];
+            int count=0;
+            for(int j=0;j<nums.size();j++){
+                if(num==nums[j]){
+                    count++;
+                }
+            }
+            if(count==1){
+                return num;
+            }
         }
-        return xor1;
+        return -1;
+        
+        // int xor1=0;
+        // for(int i=0;i<nums.size();i++){
+        //     xor1=xor1 ^ nums[i];
+        // }
+        // return xor1;
     }
 };
