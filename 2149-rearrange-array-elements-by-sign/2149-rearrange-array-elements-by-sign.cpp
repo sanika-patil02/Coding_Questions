1bb1722fd4a012b8ef7f0e0b5ee04d05d87103ab
sanika-patil02 <115109440+sanika-patil02@users.noreply.github.com>
2024-07-30 23:36:04 +0530

    Time: 153 ms (23.77%), Space: 126.4 MB (61.23%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+// Brute Force Approach-Time Complexity o(n+n/2)
+// Space Complexity o(n)
+        // int n=nums.size();
+        // vector<int>positive;
+        // vector<int>negative;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0){
+        //         positive.push_back(nums[i]);
+        //     }
+        //     else{
+        //         negative.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<positive.size();i++){
+        //     nums[2*i]=positive[i];
+        //     nums[2*i+1]=negative[i];
+        // }
+        // return nums;
+
+// Optimal Approach-Time Complexity o(n)
+// Space Complexity o(n)
+        int n=nums.size();
+        vector<int>v(n,0);
+        int pos_index=0;
+        int neg_index=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                v[pos_index]=nums[i];
+                pos_index=pos_index+2;
+            }
+            else{
+                v[neg_index]=nums[i];
+                neg_index=neg_index+2;
+            }
+        }
+        return v;
+
+// If positives are not equal to negatives
+// Brute Force Approach
+// vector<int>positive;
+// vector<int>negative;
+// for(int i=0;i<n;i++){
+//     if(nums[i]>0){
+//         positive.push_back(nums[i]);
+//     }
+//     else{
+//         negative.push_back(nums[i]);
+//     }
+// }
+// if(positive.size() > negative.size()){
+//     for(int i=0;i<negative.size();i++){
+//     nums[2*i]=positive[i];
+//     nums[2*i+1]=negative[i];
+//     }
+//     int index=negative.size() * 2;
+//     for(int j=negative.size();j<positive.size();j++){
+//         nums[index]=positive[j];
+//         index++;
+//     }
+// }
+// else{
+//     for(int i=0;i<positive.size();i++){
+//     nums[2*i]=positive[i];
+//     nums[2*i+1]=negative[i];
+//     }
+//      int index=positive.size() * 2;
+//     for(int j=positive.size();j<negative.size();j++){
+//         nums[index]=negative[j];
+//         index++;
+//     }
+// } 
+
+// return nums;
+        
+    }
+};
