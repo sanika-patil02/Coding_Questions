4208d2149a51304c82669f0562b1b01fcbdbc7f9
sanika-patil02 <115109440+sanika-patil02@users.noreply.github.com>
2024-07-31 00:15:16 +0530

    Time: 144 ms (53.10%), Space: 126.4 MB (75.57%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,40 +3,40 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
 // Brute Force Approach-Time Complexity o(n+n/2)
 // Space Complexity o(n)
-        int n=nums.size();
-        vector<int>positive;
-        vector<int>negative;
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                positive.push_back(nums[i]);
-            }
-            else{
-                negative.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<n/2;i++){
-            nums[2*i]=positive[i];
-            nums[2*i+1]=negative[i];
-        }
-        return nums;
+        // int n=nums.size();
+        // vector<int>positive;
+        // vector<int>negative;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0){
+        //         positive.push_back(nums[i]);
+        //     }
+        //     else{
+        //         negative.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<n/2;i++){
+        //     nums[2*i]=positive[i];
+        //     nums[2*i+1]=negative[i];
+        // }
+        // return nums;
 
 // Optimal Approach-Time Complexity o(n)
 // Space Complexity o(n)
-        // int n=nums.size();
-        // vector<int>v(n,0);
-        // int pos_index=0;
-        // int neg_index=1;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]>0){
-        //         v[pos_index]=nums[i];
-        //         pos_index=pos_index+2;
-        //     }
-        //     else{
-        //         v[neg_index]=nums[i];
-        //         neg_index=neg_index+2;
-        //     }
-        // }
-        // return v;
+        int n=nums.size();
+        vector<int>v(n,0);
+        int pos_index=0;
+        int neg_index=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                v[pos_index]=nums[i];
+                pos_index=pos_index+2;
+            }
+            else{
+                v[neg_index]=nums[i];
+                neg_index=neg_index+2;
+            }
+        }
+        return v;
 
 // If positives are not equal to negatives
 // (If any of the +ve & -ve no are left,add them at end without altering order)
