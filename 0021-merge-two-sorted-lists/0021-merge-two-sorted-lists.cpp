5a39cca68432ba4ffec95da542818e10b2e962a6
sanika-patil02 <115109440+sanika-patil02@users.noreply.github.com>
2024-08-07 14:07:58 +0530

    Time: 4 ms (61.51%), Space: 19.8 MB (25.94%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,192 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+// Brute-Force Approach-Time Complexity o(n1+n2)
+// Space Complexity-o(n1+n2),here n1=length of 1st Linkedlist,n2=length of 2nd Linkedlist
+        // if(list1==NULL) return list2;
+        // if(list2==NULL) return list1;
+
+        // ListNode *n=new ListNode();
+        // ListNode *head;
+        // ListNode *curr1=list1;
+        // ListNode *curr2=list2;
+        // if((curr1->val) < (curr2->val)){
+        //     n->val=curr1->val;
+        //     n->next=NULL;
+        //     head=n;
+        //     curr1=curr1->next;
+        // }
+        // else{
+        //     n->val=curr2->val;
+        //     n->next=NULL;
+        //     head=n;
+        //     curr2=curr2->next;
+        // }
+        // ListNode *curr=head;
+        // while(curr1!=NULL && curr2!=NULL){
+        //     ListNode *n=new ListNode();
+        //     if((curr1->val) < (curr2->val)){
+        //         n->val=curr1->val;
+        //         n->next=NULL;
+        //         curr->next=n;
+        //         curr=curr->next;
+        //         curr1=curr1->next;
+        //     }
+        //     else{
+        //         n->val=curr2->val;
+        //         n->next=NULL;
+        //         curr->next=n;
+        //         curr=curr->next;
+        //         curr2=curr2->next;
+        //     }
+        // }
+        // while(curr1!=NULL){
+        //     ListNode *n=new ListNode();
+        //     n->val=curr1->val;
+        //     n->next=NULL;
+        //     curr->next=n;
+        //     curr=curr->next;
+        //     curr1=curr1->next;
+        // }
+        //  while(curr2!=NULL){
+        //     ListNode *n=new ListNode();
+        //     n->val=curr2->val;
+        //     n->next=NULL;
+        //     curr->next=n;
+        //     curr=curr->next;
+        //     curr2=curr2->next;
+        // }
+        // return head;
+
+// OR
+
+        // if(list1==NULL) return list2;
+        // if(list2==NULL) return list1;
+
+        // ListNode *dummy=new ListNode();
+        // dummy->val=0;
+        // dummy->next=NULL;
+        // ListNode *curr=dummy;
+        // ListNode *curr1=list1;
+        // ListNode *curr2=list2;
+
+        // while(curr1!=NULL && curr2!=NULL){
+        //     ListNode *n=new ListNode();
+        //     if((curr1->val) < (curr2->val)){
+        //         n->val=curr1->val;
+        //         n->next=NULL;
+        //         curr->next=n;
+        //         curr=curr->next;
+        //         curr1=curr1->next;
+        //     }
+        //     else{
+        //        n->val=curr2->val;
+        //        n->next=NULL;
+        //        curr->next=n;
+        //        curr=curr->next;
+        //        curr2=curr2->next;
+        //     }
+        // }
+
+        // while(curr1!=NULL){
+        //     ListNode *n=new ListNode();
+        //     n->val=curr1->val;
+        //     n->next=NULL;
+        //     curr->next=n;
+        //     curr=curr->next;
+        //     curr1=curr1->next;
+        // }
+
+        // while(curr2!=NULL){
+        //     ListNode *n=new ListNode();
+        //     n->val=curr2->val;
+        //     n->next=NULL;
+        //     curr->next=n;
+        //     curr=curr->next;
+        //     curr2=curr2->next;
+        // }
+        
+        // ListNode *head=dummy->next;
+        // delete dummy;
+
+        // return head;
+
+// Optimal Approach-Time Complexity o(n1)+o(n2)
+// Space Complexity-o(1)
+
+
+// if(list1==NULL) return list2;
+// if(list2==NULL) return list1; 
+
+// ListNode *dummy=new ListNode();
+// dummy->val=0;
+// dummy->next=NULL;
+// ListNode *curr=dummy;
+// ListNode *curr1=list1;
+// ListNode *curr2=list2;
+
+// while(curr1!=NULL && curr2!=NULL){
+//     if((curr1->val) < (curr2->val)){
+//         curr->next=curr1;
+//         curr=curr->next;
+//         curr1=curr1->next;
+//     }
+//     else{
+//         curr->next=curr2;
+//         curr=curr->next;
+//         curr2=curr2->next;
+//     }
+// }
+// while(curr1!=NULL){
+//     curr->next=curr1;
+//     curr=curr->next;
+//     curr1=curr1->next;
+// }
+// while(curr2!=NULL){
+//     curr->next=curr2;
+//     curr=curr->next;
+//     curr2=curr2->next;
+// }
+// ListNode *head=dummy->next;
+// delete dummy;
+
+// return head;
+
+
+// OR
+// l1 points to smaller no
+ListNode *l1=list1;
+ListNode *l2=list2;
+
+if(l1==NULL) return l2;
+if(l2==NULL) return l1;
+
+if((l1->val) > (l2->val)){
+    swap(l1,l2);
+}
+ListNode *res=l1;
+
+while(l1!=NULL && l2!=NULL){
+ListNode *prev=NULL;
+while(l1!=NULL && (l1->val) <= (l2->val)){
+    prev=l1;
+    l1=l1->next;
+}
+prev->next=l2;
+
+swap(l1,l2);
+
+}
+    return res;
+    }
+};
